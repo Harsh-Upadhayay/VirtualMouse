4e4f0c9698a82bcac093cb37432619428420f7ae
Harsh <harshupadhayay906@gmail.com>
2021-09-09 16:03:08 +0530

    "Updates"

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import cv2
 import numpy
-
-
+import time
+import HandTrackingModule
 cap = cv2.VideoCapture(0)
 cap.set(3, 640)
 cap.set(4, 480)
+pTime = 0
+detector = HandTrackingModule.handDetector(maxHands=1)
 
 while True:
     success, img = cap.read()
+    img = cv2.flip(img, 1)
+    # img = detector.findHands(img)
+    # lmList, bbox = detector.findPosition(img)
+    cTime = time.time()
+    fps = 1 / (cTime-pTime)
+    pTime = cTime
+    cv2.putText(img, str(int(fps)), (20, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
     cv2.imshow("Image", img)
     cv2.waitKey(1)
