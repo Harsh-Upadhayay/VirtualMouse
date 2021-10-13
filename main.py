90601c0fbac851f746612de17718735c621c1bce
Harsh Upadhayay <harshupadhayay906@gmail.com>
2021-10-14 00:12:09 +0530

    Added finger up/down status check

--- main.py
+++ main.py
@@ -2,17 +2,26 @@ import cv2
 import numpy
 import time
 import HandTrackingModule
-cap = cv2.VideoCapture(0)
+cap =  cv2.VideoCapture(0, cv2.CAP_DSHOW)
 cap.set(3, 640)
 cap.set(4, 480)
 pTime = 0
-detector = HandTrackingModule.handDetector(maxHands=2)
+detector = HandTrackingModule.handDetector(maxHands=1)
 
 while True:
     success, img = cap.read()
     img = cv2.flip(img, 1)
     img = detector.findHands(img)
     lmList, bbox = detector.findPosition(img)
+
+    x1,y1,x2,y2 = 0,0,0,0
+    if len(lmList) != 0:
+        x1, y1 = lmList[8][1:]
+        x2, y2 = lmList[12][1:]
+
+    fingers = detector.fingersUp()
+    print(fingers)
+    #print(x1, y1, x2, y2)
     cTime = time.time()
     fps = 1 / (cTime-pTime)
     pTime = cTime
